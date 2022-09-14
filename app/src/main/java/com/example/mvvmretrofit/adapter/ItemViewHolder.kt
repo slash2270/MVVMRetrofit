24b92eb19c23b4e58cb136dfceeebc10ed5569e0
slash <tw027slash@gmail.com>
2022-09-14 16:17:18 +0800

    09/14

--- app/src/main/java/com/example/mvvmretrofit/adapter/ItemViewHolder.kt
+++ app/src/main/java/com/example/mvvmretrofit/adapter/ItemViewHolder.kt
@@ -5,7 +5,6 @@ import com.example.mvvmretrofit.BR
 import com.example.mvvmretrofit.databinding.MainListItemBinding
 
 class ItemViewHolder(var mainListItemBinding: MainListItemBinding) : RecyclerView.ViewHolder(mainListItemBinding.root) {
-
     fun bindItem(obj: Any?) {
         mainListItemBinding.setVariable(BR.item, obj)
         mainListItemBinding.executePendingBindings()
